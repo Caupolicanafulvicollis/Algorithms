4cbbcc6f76473c35694e3b4e5af064e641d4f490
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-06-27 21:52:54 -0400

    Update Search_linear.py

--- 1.Searching_algorithms/1.Search_linear/Search_linear.py
+++ 1.Searching_algorithms/1.Search_linear/Search_linear.py
@@ -1,13 +1,16 @@
-def busquedaSecuencial(lista,item):
+def linear_search(lista,elemento):
     position=0
     encontrado=False
-
-    while position < len(lista) and not encontrado:
-        if lista[position] == item:
+    pasos=0
+    while elemento<len(lista) and not encontrado:
+        pasos+=1
+        if lista[position]==elemento:
             encontrado=True
         else:
             position += 1
     print(encontrado,position)
 
-lista=[1, 2, 32, 8, 17, 19, 42, 13, 0]
-print(busquedaSecuencial(lista, 13))
+if __name__ == '__main__':
+    lista=[1,3,5,7,9,11,13,15,17,19,21,23]
+    elemento=19
+    linear_search(lista,elemento)
