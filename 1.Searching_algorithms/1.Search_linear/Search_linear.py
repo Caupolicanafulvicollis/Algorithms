48c8e0783bbcbec7a305aa98e26d30ffb7d62969
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-06-27 22:29:11 -0400

    Update Search_linear.py

--- 1.Searching_algorithms/1.Search_linear/Search_linear.py
+++ 1.Searching_algorithms/1.Search_linear/Search_linear.py
@@ -2,24 +2,18 @@ def search_linear(lista, elemento):
     posicion = 0
     encontrado = 0
     pasos = 0
-
-    while posicion < len(lista):
-        pasos += 1
-
+    while posicion<len(lista):
+        pasos+=1
+        print(f"DEBUG: 'elemento:'{elemento}' | 'posicion:'{posicion}' | 'paso:'{pasos}")
         if lista[posicion] == elemento:
             encontrado = 1
+            print(f"El elemento {elemento} se encuentra en la posición {posicion}. Se encontró en {pasos} pasos.")
             break
-
+        else:
+            print("El elemento no se encontró en la lista.")
         posicion += 1
 
-    if encontrado == 1:
-        return f"El elemento {elemento} se encuentra en la posición {posicion}. Se encontró en {pasos} pasos."
-    else:
-        return "El elemento no se encontró en la lista."
-
-# Ejemplo de uso
-mi_lista =[1,3,5,7,9,11,13,15,17,19,21,23]
-mi_elemento =19
-
-resultado=search_linear(mi_lista, mi_elemento)
-print(resultado)
+if __name__ == "__main__":
+    lista=[1,3,5,7,9,11,13,15,17,19,21,23]
+    elemento=5
+    search_linear(lista, elemento)
