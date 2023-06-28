d8e2d7af705e9c46c8fc649864c0fe26beb9ffc9
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-06-27 21:14:23 -0400

    Update Binary_search.py

--- 1.Searching_algorithms/2.Binary_search/Binary_search.py
+++ 1.Searching_algorithms/2.Binary_search/Binary_search.py
@@ -1,14 +1,16 @@
 def binary_search(lista,elemento):
     izq=0
     der=len(lista)-1
-    pos=-1 # Inicializo respuesta, el valor no fue encontrado
+    pasos=0
+    # Inicializo respuesta, el valor no fue encontrado
     while izq <= der:
-        #valor punto medio del segmento
-        medio=(der+izq)//2
+        pasos+=1
+        medio=(izq+der)//2
         print(f"DEBUG: 'izq:'{izq}' | 'der:'{der}' | 'medio:'{medio}")
         #si el medio es igual al valor buscado, lo devuleve
         if lista[medio]==elemento:
-            return medio
+            print(f'Valor encontrado en {pasos} pasos, en la posicion {medio}')
+            return True
         #si el valor del punto medio es mayor que 'elemento', sigue buscando
         #en el segmento de la izquierda: [izq, medio-1], descartando la derecha
         elif lista[medio]>elemento:
@@ -18,12 +20,11 @@ def binary_search(lista,elemento):
         elif lista[medio]<elemento:
             izq=medio+1
         # si no salió del ciclo, vuelve a iterar con el nuevo segmento
-    return pos
+    print(f'Valor no encontrado')
+    return False
 
 if __name__ == '__main__':
-    lista=input('Dame una listaa ordenada ([[]] para terminar): ')
-    while lista !=[[]]:
-        elemento= input('Valor buscado?: ')
-        resultado=binary_search(lista,elemento)
-        print(f"Resultado: {resultado}")
-        lista=input('Dame una lista ordenada ([[]] para terminar): ')
+    lista=[2, 5, 7, 12, 18, 20, 27, 30, 35]
+    elemento=5
+    posicion=binary_search(lista,elemento)
+    print(posicion)
