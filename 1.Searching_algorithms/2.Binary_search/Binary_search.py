c16c4015e885c8918f346c38568777079a00c772
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-06-27 21:55:05 -0400

    Update Binary_search.py

--- 1.Searching_algorithms/2.Binary_search/Binary_search.py
+++ 1.Searching_algorithms/2.Binary_search/Binary_search.py
@@ -2,6 +2,7 @@ def binary_search(lista,elemento):
     izq=0
     der=len(lista)-1
     pasos=0
+    encontrado=False
     while izq <= der:
         pasos+=1
         medio=(der+izq)//2
@@ -9,7 +10,7 @@ def binary_search(lista,elemento):
         #si el medio es igual al valor buscado, lo devuleve
         if lista[medio]==elemento:
             print(f'Valor encontrado en {pasos} pasos, en la posicion {medio}')
-            return True
+            return encontrado==True
         #si el valor del punto medio es mayor que 'elemento', sigue buscando
         #en el segmento de la izquierda: [izq, medio-1], descartando la derecha
         elif lista[medio]>elemento:
@@ -20,9 +21,9 @@ def binary_search(lista,elemento):
             izq=medio+1
         # si no salió del ciclo, vuelve a iterar con el nuevo segmento
     print(f'Valor no encontrado')
-    return False
+    return encontrado
 
 if __name__ == '__main__':
     lista=[1,3,5,7,9,11,13,15,17,19,21,23]
-    elemento=19
+    elemento=3
     binary_search(lista,elemento)
