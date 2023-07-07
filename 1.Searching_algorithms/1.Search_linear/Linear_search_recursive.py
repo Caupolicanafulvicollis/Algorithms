b16ad764956e2177c45188cc2bbf2c5f83362770
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-07-06 23:36:42 -0400

    Update Linear_search_recursive.py

--- 1.Searching_algorithms/1.Search_linear/Linear_search_recursive.py
+++ 1.Searching_algorithms/1.Search_linear/Linear_search_recursive.py
@@ -1,15 +1,15 @@
-def linear_search_recursive(lista, indice, elemento):
-  indice=0
-  encontrado=False
-  pasos=0
+def linear_search_recursive(lista, indice, elemento,pasos=0):
+  print(f"DEBUG: 'elemento:'{elemento}' | 'indice:'{indice}' | 'paso:'{pasos}")
   if indice>=len(lista):
-    return -1
     print(f"El elemento {elemento} no se encontro en la lista.")
+    return -1
   if lista[indice]==elemento:
     print(f"El elemento {elemento} se encuentra en la posición {indice}. Se encontró en {pasos} pasos.")
-  else:
-    return linear_search_recursive(lista, indice+1, elemento)
-  pasos+=1
+    pasos+=1
+  elif lista[indice]!=elemento:
+    return linear_search_recursive(lista, indice+1, elemento, pasos+1)
+  
+  
 
 if __name__=="__main__":
     lista=[1,3,5,7,9,11,13,15,17,19,21,23]
