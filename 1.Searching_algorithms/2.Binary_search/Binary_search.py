b888c8a4441943ed2c0b9c24098eb189dca92a18
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-07-11 02:40:16 -0400

    Update Binary_search.py

--- 1.Searching_algorithms/2.Binary_search/Binary_search.py
+++ 1.Searching_algorithms/2.Binary_search/Binary_search.py
@@ -1,26 +1,27 @@
 def binary_search(lista, elemento):
-    izq=0
     der=len(lista)-1
-    encontrado=False
+    izq=0
     pasos=0
-
+    encontrado=False
     while izq<=der:
         pasos+=1
         medio=(der+izq)//2
-        print(f"DEBUG: 'izquierda:'{izq}' | 'derecha:'{der}' | 'pasos:'{pasos} | 'medio:'{medio}")
-        if lista[medio] == elemento:
-            print(f"El elemento {elemento} se encuentra en la posición {medio}. Se encontró en {pasos} pasos.")
-            encontrado=True
+        print(f"DEBUG: 'derecha: {der}' | 'izquierda: {izq}' | 'pasos: {medio}' | 'elemento {elemento}'")
+        encontrado=True
+        if lista[medio]==elemento:
+            print(f"elemento {elemento} se encuentra en la posicion {medio}, el algoritmo lo ubico en {pasos} pasos")
+            encontrado= True
             break
         elif lista[medio]>elemento:
             der=medio-1
-            print(f"DEBUG: 'izquierda: {izq} | 'cambia valor de la derecha a: {der}' | 'pasos:{pasos}'")
+            print(f"DEBUG: 'derecha: {der}' | 'el valor de la izquierda se cambia a: {izq}' | 'pasos: {pasos}' | 'elemento {elemento}'")
         elif lista[medio]<elemento:
             izq=medio+1
-            print(f"DEBUG: 'cambia valor de la izquierda a: {izq} | 'derecha: {der}' | 'pasos:{pasos}'")
+            print(f"DEBUG: 'el valor de la derecha se cambia a: {der}' | 'izquierda: {izq}' | 'pasos: {pasos}' | 'elemento {elemento}'")
     if not encontrado:
-        print(f"El elemento {elemento} no se encuentra en la lista.")
+        print(f"el elemento {elemento} no esta en la lista")  
+
 if __name__ == "__main__":
     lista=[1,3,5,7,9,11,13,15,17,19,21,23]
-    elemento=22
+    elemento=21
     binary_search(lista, elemento)
