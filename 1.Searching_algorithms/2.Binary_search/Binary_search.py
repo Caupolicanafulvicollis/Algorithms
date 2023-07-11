4b2b81787039fe50c0b5eb5c3ab5d53df62a0399
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-07-11 01:50:41 -0400

    Update Binary_search.py

--- 1.Searching_algorithms/2.Binary_search/Binary_search.py
+++ 1.Searching_algorithms/2.Binary_search/Binary_search.py
@@ -1,29 +1,26 @@
-def binary_search(lista,elemento):
+def binary_search(lista, elemento):
     izq=0
     der=len(lista)-1
-    pasos=0
     encontrado=False
-    while izq <= der:
+    pasos=0
+
+    while izq<=der:
         pasos+=1
         medio=(der+izq)//2
-        print(f"DEBUG: 'izq:'{izq}' | 'der:'{der}' | 'medio:'{medio}")
-        #si el medio es igual al valor buscado, lo devuleve
-        if lista[medio]==elemento:
-            print(f'Valor encontrado en {pasos} pasos, en la posicion {medio}')
-            return encontrado==True
-        #si el valor del punto medio es mayor que 'elemento', sigue buscando
-        #en el segmento de la izquierda: [izq, medio-1], descartando la derecha
+        print(f"DEBUG: 'izquierda:'{izq}' | 'derecha:'{der}' | 'pasos:'{pasos} | 'medio:'{medio}")
+        if lista[medio] == elemento:
+            print(f"El elemento {elemento} se encuentra en la posición {medio}. Se encontró en {pasos} pasos.")
+            encontrado=True
+            break
         elif lista[medio]>elemento:
             der=medio-1
-        # sino, sigue buscando en el segmento de la derecha:
-        # [medio+1, der], descartando la izquierda
+            print(f"DEBUG: 'izquierda: {izq} | 'cambia valor de la derecha a: {der}' | 'pasos:{pasos}'")
         elif lista[medio]<elemento:
             izq=medio+1
-        # si no salió del ciclo, vuelve a iterar con el nuevo segmento
-    print(f'Valor no encontrado')
-    return encontrado
-
-if __name__ == '__main__':
+            print(f"DEBUG: 'cambia valor de la izquierda a: {izq} | 'derecha: {der}' | 'pasos:{pasos}'")
+    if not encontrado:
+        print(f"El elemento {elemento} no se encuentra en la lista.")
+if __name__ == "__main__":
     lista=[1,3,5,7,9,11,13,15,17,19,21,23]
-    elemento=3
-    binary_search(lista,elemento)
+    elemento=22
+    binary_search(lista, elemento)
