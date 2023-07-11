c6d312d120e14308bf6696e04a6914f8d4bd9c76
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-07-11 00:24:37 -0400

    Update Search_linear.py

--- 1.Searching_algorithms/1.Search_linear/Search_linear.py
+++ 1.Searching_algorithms/1.Search_linear/Search_linear.py
@@ -1,17 +1,16 @@
 def linear_search(lista, elemento):
     posicion = 0
-    encontrado = False
-    pasos=0 
-    
+    pasos=0
+    encontrado=0
     while posicion < len(lista):
         pasos+=1
-        print(f"DEBUG: 'elemento:'{elemento}' | 'posicion:'{posicion}' | 'paso:'{pasos}")
+        print(f"DEBUG: 'elemento:{elemento}' | 'posicion:{posicion}' | 'pasos:{pasos}'")
         if lista[posicion] == elemento:
-            print(f"el elemento {elemento} se encuentra en la posicion de {posicion}, se encontro en el paso {pasos}")
-            encontrado==True
+            print(f"se ha encontrado el {elemento} en la posicion {posicion}, en {pasos} pasos" )
             break
-        else:
-            posicion+=1
+        elif posicion+1 == len(lista):
+            print(f"el elemento {elemento} no fue encontrado en la lista")
+        posicion+=1
 
 if __name__ == '__main__':
     lista=[1,3,5,7,9,11,13,15,17,19,21,23]
