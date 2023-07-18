2b72bc5da778420d81239322b19a315800c7f1e7
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-07-17 20:27:31 -0400

    Update Jump_search.py

--- 1.Searching_algorithms/4.Jump_search/Jump_search.py
+++ 1.Searching_algorithms/4.Jump_search/Jump_search.py
@@ -24,3 +24,29 @@ def jump_search(A, item):
     B = A[i:i+m]                        # Step 5
     print("Processing Block - {}".format(B))
     return linear_search(B, item, i)
+
+import math
+def jump_search(lista,elemento,):
+  left=0
+  right=len(lista)-1
+  pasos=math.sqrt(n)
+  encontrado=False
+  while lista[right]!=lista[left] and lista[left]<=elemento<=lista[right]:
+    mid=left+(elemento-lista[left])*(right-left)//(lista[right]-lista[left])
+    pasos=+1
+    print(f"DEBUG: 'left:'{left}' | 'right:'{right}' | 'medio:'{mid}")
+    if elemento == lista[mid]:
+      print(f'Valor encontrado en {pasos} pasos, en la posicion {mid}')
+      encontrado==True
+      break
+    elif elemento<lista[mid]:
+      right=mid-1
+    else:
+      left=mid+1
+  print(f'Valor {elemento} no encontrado')
+  return encontrado
+
+if __name__ =='__main__':
+  lista=[1,3,5,7,9,11,13,15,17,19,21,23]
+  elemento=21
+  jump_search(lista,elemento)
