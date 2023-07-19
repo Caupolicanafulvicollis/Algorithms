222594236838830fc3a929f1764ab8ab41a1aaa4
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-07-18 21:55:25 -0400

    Update Jump_search.py

--- 1.Searching_algorithms/4.Jump_search/Jump_search.py
+++ 1.Searching_algorithms/4.Jump_search/Jump_search.py
@@ -1,38 +1,28 @@
-'''
-Jump Search function
-Arguments:
-A    - The source list
-item - Element for which the index needs to be found
-'''
 import math
+def jump_search(lista, elemento):
+  pasos=math.sqrt(len(lista))
 
-def jump_search(A, item):
-    print("Entering Jump Search")
-    n = len(A)                          # Length of the array
-    m = int(math.sqrt(n))               # Step length
-    i = 0                               # Starting interval
-
-    while i != len(A)-1 and A[i] < item:
-        print("Processing Block - {}".format(A[i: i+m]))
-        if A[i+m-1] == item:            # Found the search key
-            return i+m-1
-        elif A[i+m-1] > item:           # Linear search for key in block
-            B = A[i: i+m-1]
-            return linear_search(B, item, i)
-        i += m
+  while i != len(A)-1 and A[i] < item:
+    print("Processing Block - {}".format(A[i: i+m]))
+    if A[i+m-1] == item:            # Found the search key
+      return i+m-1
+    elif A[i+m-1] > item:           # Linear search for key in block
+      B = A[i: i+m-1]
+      return linear_search(B, item, i)
+      i += m
 
     B = A[i:i+m]                        # Step 5
     print("Processing Block - {}".format(B))
     return linear_search(B, item, i)
 
 import math
-def jump_search(lista,elemento,):
+def jump_search(lista,elemento):
   left=0
   right=len(lista)-1
   pasos=math.sqrt(n)
   encontrado=False
-  while lista[right]!=lista[left] and lista[left]<=elemento<=lista[right]:
-    mid=left+(elemento-lista[left])*(right-left)//(lista[right]-lista[left])
+  while :
+
     pasos=+1
     print(f"DEBUG: 'left:'{left}' | 'right:'{right}' | 'medio:'{mid}")
     if elemento == lista[mid]:
