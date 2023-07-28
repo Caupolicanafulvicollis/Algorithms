fa9a84515f60f9d2d6a4a8fa54bf93f863dbfe30
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-07-27 20:47:45 -0400

    Update Interpolation_Search.py

--- 1.Searching_algorithms/3.Interpolation_Search/Interpolation_Search.py
+++ 1.Searching_algorithms/3.Interpolation_Search/Interpolation_Search.py
@@ -20,5 +20,5 @@ def interpolation_search(lista,elemento):
 
 if __name__ =='__main__':
   lista=[1,3,5,7,9,11,13,15,17,19,21,23]
-  elemento=21
+  elemento=5
   interpolation_search(lista,elemento)
