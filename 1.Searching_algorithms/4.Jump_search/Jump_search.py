013fb43492766a5a18dbb26c5d39bf31d0e40376
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-08-03 22:32:40 -0400

    Update Jump_search.py

--- 1.Searching_algorithms/4.Jump_search/Jump_search.py
+++ 1.Searching_algorithms/4.Jump_search/Jump_search.py
@@ -1,40 +1,21 @@
-import math
-def jump_search(lista, elemento):
-  pasos=math.sqrt(len(lista))
-
-  while i != len(A)-1 and A[i] < item:
-    print("Processing Block - {}".format(A[i: i+m]))
-    if A[i+m-1] == item:            # Found the search key
-      return i+m-1
-    elif A[i+m-1] > item:           # Linear search for key in block
-      B = A[i: i+m-1]
-      return linear_search(B, item, i)
-      i += m
-
-    B = A[i:i+m]                        # Step 5
-    print("Processing Block - {}".format(B))
-    return linear_search(B, item, i)
-
-import math
+import math 
 def jump_search(lista,elemento):
-  left=0
-  right=len(lista)-1
-  pasos=math.sqrt(n)
-  encontrado=False
-  while :
-
-    pasos=+1
-    print(f"DEBUG: 'left:'{left}' | 'right:'{right}' | 'medio:'{mid}")
-    if elemento == lista[mid]:
-      print(f'Valor encontrado en {pasos} pasos, en la posicion {mid}')
-      encontrado==True
-      break
-    elif elemento<lista[mid]:
-      right=mid-1
-    else:
-      left=mid+1
-  print(f'Valor {elemento} no encontrado')
-  return encontrado
+    step=math.sqrt(len(lista))
+    last_index=0
+    while lista[int(min(step,len(lista)-1))]<elemento:
+       last_index=step
+       step+=math.sqrt(len(lista))
+       if last_index>=len(lista):
+          return -1
+    
+    while lista[int(last_index)]<elemento:
+       last_index+=1
+       if last_index==min(step,len(lista)):
+            return-1
+    if lista[int(last_index)]==elemento:
+       return last_index
+    return-1
+    print(f'Valor encontrado en {elemento}')
 
 if __name__ =='__main__':
   lista=[1,3,5,7,9,11,13,15,17,19,21,23]
