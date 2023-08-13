97993181fe5d2512d8af085e34cd436006d5ed2e
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-08-13 19:35:27 -0400

    Update Jump_search.py

--- 1.Searching_algorithms/4.Jump_search/Jump_search.py
+++ 1.Searching_algorithms/4.Jump_search/Jump_search.py
@@ -21,3 +21,26 @@ if __name__ =='__main__':
   lista=[1,3,5,7,9,11,13,15,17,19,21,23]
   elemento=21
   jump_search(lista,elemento)
+////////////////////////////
+import math 
+def jump_search(lista,elemento):
+    step=math.sqrt(len(lista))
+    step=0
+    while lista[int(min(step,len(lista)-1))]<elemento:
+       step+=math.sqrt(len(lista))
+       if step>=len(lista):
+        print(f"DEBUG: 'step:'{step}' | 'right:'{right}' | 'medio:'{mid}")
+        return -1
+    while lista[int(step)]<elemento:
+        step+=1
+        if step==min(step,len(lista)):
+            return-1
+    if lista[int(step)]==elemento:
+        print(f'Valor encontrado {elemento} fue encontrado en {step} pasos, en la poscion ')
+        return step
+    
+
+if __name__ =='__main__':
+  lista=[1,3,5,7,9,11,13,15,17,19,21,23]
+  elemento=21
+  jump_search(lista,elemento)
