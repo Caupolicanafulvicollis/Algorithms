8020c28ee27c376b393bee08452178faa8bb192c
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-08-14 19:11:31 -0400

    Update Jump_search.py

--- 1.Searching_algorithms/4.Jump_search/Jump_search.py
+++ 1.Searching_algorithms/4.Jump_search/Jump_search.py
@@ -21,7 +21,9 @@ if __name__ =='__main__':
   lista=[1,3,5,7,9,11,13,15,17,19,21,23]
   elemento=21
   jump_search(lista,elemento)
-////////////////////////////
+
+
+
 import math 
 def jump_search(lista,elemento):
     step=math.sqrt(len(lista))
@@ -38,46 +40,3 @@ def jump_search(lista,elemento):
     if lista[int(step)]==elemento:
         print(f'Valor encontrado {elemento} fue encontrado en {step} pasos, en la poscion ')
         return step
-////////////////////////////////
-
-import math 
-def jump_search(lista,elemento):
-    step=math.sqrt(len(lista))
-    step=0
-    while lista[int(min(step,len(lista)-1))]<elemento:
-       step+=math.sqrt(len(lista))
-       if step>=len(lista):
-        print(f"DEBUG: 'step:'{step}' | 'right:'{right}' | 'medio:'{mid}")
-        return linear_search(lista,elemento)
-    while lista[int(step)]<elemento:
-        step+=1
-        if step==min(step,len(lista)):
-            return-1
-    if lista[int(step)]==elemento:
-        print(f'Valor encontrado {elemento} fue encontrado en {round(step)} pasos, en la posicion {lista.index(elemento)}')
-        return step
-
-
-def linear_search(lista, elemento):
-    posicion = 0
-    pasos=0
-    encontrado=0
-    while posicion < len(lista):
-        pasos+=1
-        print(f"DEBUG: 'elemento:{elemento}' | 'posicion:{posicion}' | 'pasos:{pasos}'")
-        if lista[posicion] == elemento:
-            print(f"se ha encontrado el {elemento} en la posicion {posicion}, en {pasos} pasos")
-        elif posicion == len(lista):
-            print(f"el elemento {elemento} no fue encontrado en la lista")
-        posicion+=1
-
-if __name__ =='__main__':
-  lista=[1,3,5,7,9,11,13,15,17,19,21,23]
-  elemento=21
-  jump_search(lista,elemento)
-
-
-if __name__ =='__main__':
-  lista=[1,3,5,7,9,11,13,15,17,19,21,23]
-  elemento=21
-  jump_search(lista,elemento)
