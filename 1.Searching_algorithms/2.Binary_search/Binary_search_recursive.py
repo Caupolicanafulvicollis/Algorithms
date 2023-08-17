1f182f164baa135455893ad0c2ef2c90898cca12
gnvidal <45744107+gnvidal@users.noreply.github.com>
2023-08-16 22:10:43 -0400

    Update Binary_search_recursive.py

--- 1.Searching_algorithms/2.Binary_search/Binary_search_recursive.py
+++ 1.Searching_algorithms/2.Binary_search/Binary_search_recursive.py
@@ -10,14 +10,15 @@ def binary_search_recursive(lista, elemento,  left=0, right=None, pasos=0):
       print(f"elemento {elemento} se encuentra en la posicion {medio}, el algoritmo lo ubico en {pasos} pasos")
       encontrado=True
     elif lista[medio]>elemento:
-        print(f"DEBUG2: 'izquierda: {left}' | 'el valor de la derecha se cambia a: {right}' | 'pasos: {pasos}' | 'elemento {elemento}'")
+        print(f"DEBUG2: 'izquierda: {left}' | 'el valor de la derecha se cambia a: {medio-1}' | 'pasos: {pasos}' | 'elemento {elemento}'")
         return binary_search_recursive(lista, elemento, left, medio-1, pasos)
     elif lista[medio]<elemento:
-      return binary_search_recursive(lista, elemento, medio+1, right, pasos)
+        print(f"DEBUG2: 'derecha: {right}' | 'el valor de la izquierda se cambia a: {medio+1}' | 'pasos: {pasos}' | 'elemento {elemento}'")
+        return binary_search_recursive(lista, elemento, medio+1, right, pasos)
   if not encontrado:
     print(f"el elemento {elemento} no esta en la lista")  
 
 if __name__ == "__main__":
   lista=[1,3,5,7,9,11,13,15,17,19,21,23]
-  elemento=2
+  elemento=21
   binary_search_recursive(lista, elemento)
