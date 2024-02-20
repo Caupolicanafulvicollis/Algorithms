0a7965b0a5313e42f636b5ebbd664023632eca71
gnvidal <guillermo.vidal.astudillo@gmail.com>
2024-02-20 01:39:42 -0300

    Create 14.ejercicio.py

---
+++ 400 ejercicios python/14.ejercicio.py
@@ -0,0 +1,9 @@
+"""Crea un función EscribirCentrado, que reciba como parámetro un texto y lo escriba centrado en pantalla (suponiendo una anchura de 80 columnas;
+pista: deberás escribir 40 - longitud/2 espacios antes del texto). Además subraya el mensaje utilizando el carácter =."""
+
+def EscribirCentrado(text):
+    text=input("inserte aqui el texto: ")
+    
+
+if __name__=="__main__":
+    EscribirCentrado()
