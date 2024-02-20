8c7fa91692a0bad8f8cfd78432020b76b8e6608e
gnvidal <guillermo.vidal.astudillo@gmail.com>
2024-02-20 01:04:51 -0300

    Update 12.ejercicio.py

--- 400 ejercicios python/12.ejercicio.py
+++ 400 ejercicios python/12.ejercicio.py
@@ -5,3 +5,14 @@
 * * * *
 * * * * *"""
 
+def main():
+    largo=1
+    alto=5
+    
+    for largo in range(largo,alto+1):
+        print("*"*largo, end="")
+        largo+1
+        print()
+        
+if __name__=="__main__":
+    main()
