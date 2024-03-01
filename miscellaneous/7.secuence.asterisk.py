b1b588a420c72e5c36808d93909ff21b2eb2fcda
gnvidal <guillermo.vidal.astudillo@gmail.com>
2024-03-01 08:15:19 -0300

    Update 7.secuence.asterisk.py

--- miscellaneous/7.secuence.asterisk.py
+++ miscellaneous/7.secuence.asterisk.py
@@ -1,5 +1,8 @@
 def main():
     
+    
 
 if __name__ == '__main__':
-main()
+    width=int(input("Ingrese el ancho de su figura: "))
+    high=int(input("Ingrese el alto de su figura: "))
+    main()
