e998583220f9e89935c84a98c90aef25e4e7ff42
gnvidal <guillermo.vidal.astudillo@gmail.com>
2024-03-13 15:22:28 -0300

    Update 7.secuence.asterisk.py

---
+++ miscellaneous/7.secuence.asterisk.py
@@ -0,0 +1,9 @@
+def main():
+    n=int(input('Ingrese un número: '))
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print('*',end=' ')
+        print()
+
+if __name__ == '__main__':
+    main()
