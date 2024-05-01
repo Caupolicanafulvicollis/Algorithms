6a5f63902f153f7989ab6a98f402c3a203e9a352
gnvidal <guillermo.vidal.astudillo@gmail.com>
2024-05-01 17:31:42 -0400

    roman

--- 06.Recursive/5.exercises.py
+++ 06.Recursive/5.exercises.py
@@ -1,1 +1,36 @@
 #Desarrollar una función que permita convertir un número romano en un número decimal.
+
+def roman_to_decimal(roman_number):
+    roman_number = roman_number.upper()
+    decimal_number = 0
+    for i in range(len(roman_number)):
+        if roman_number[i] == 'I':
+            decimal_number += 1
+            if i < len(roman_number) - 1 and roman_number[i + 1] == 'V':
+                decimal_number -= 2
+                i += 1
+                continue
+            if i < len(roman_number) - 1 and roman_number[i + 1] == 'X':
+                decimal_number -= 20
+                i += 1
+                continue
+            if i < len(roman_number) - 1 and roman_number[i + 1] == 'L':
+                decimal_number -= 50
+                i += 1
+                continue
+            if i < len(roman_number) - 1 and roman_number[i + 1] == 'C':
+                decimal_number -= 100
+                i += 1
+                continue
+            if i < len(roman_number) - 1 and roman_number[i + 1] == 'D':
+                decimal_number -= 500
+                i += 1
+                continue
+            if i < len(roman_number) - 1 and roman_number[i + 1] == 'M':
+                decimal_number -= 1000
+                i += 1
+                continue
+
+if __name__ == '__main__':
+    roman_number = input('Ingrese un número romano: ')
+    print(roman_to_decimal(roman_number))
