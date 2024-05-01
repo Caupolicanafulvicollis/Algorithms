ae52e96a2f998988a546547d6a691f4e8fac3911
gnvidal <guillermo.vidal.astudillo@gmail.com>
2024-05-01 18:02:54 -0400

    Update 5.exercises.py

--- 06.Recursive/5.exercises.py
+++ 06.Recursive/5.exercises.py
@@ -1,37 +1,20 @@
 #Desarrollar una función que permita convertir un número romano en un número decimal.
 
-def roman_to_decimal(roman_number):
-    roman_number = roman_number.upper()
-    decimal_number = 0
-    for i in range(len(roman_number)):
-        if roman_number[i] == 'I':
-            decimal_number += 1
-            if i < len(roman_number) - 1 and roman_number[i + 1] == 'V':
-                decimal_number -= 2
-                i += 1
-                continue
-            if i < len(roman_number) - 1 and roman_number[i + 1] == 'X':
-                decimal_number -= 20
-                i += 1
-                continue
-            if i < len(roman_number) - 1 and roman_number[i + 1] == 'L':
-                decimal_number -= 50
-                i += 1
-                continue
-            if i < len(roman_number) - 1 and roman_number[i + 1] == 'C':
-                decimal_number -= 100
-                i += 1
-                continue
-            if i < len(roman_number) - 1 and roman_number[i + 1] == 'D':
-                decimal_number -= 500
-                i += 1
-                continue
-            if i < len(roman_number) - 1 and roman_number[i + 1] == 'M':
-                decimal_number -= 1000
-                i += 1
-                continue
+def romano_a_decimal(romano):
+    valores_romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    decimal = 0
+    previo = 0
+
+    for letra in romano[::-1]:
+        valor = valores_romanos[letra]
+        if valor < previo:
+            decimal -= valor
+        else:
+            decimal += valor
+        previo = valor
+
+    return decimal
 
 if __name__ == '__main__':
-    #roman_number = input('Ingrese un número romano: ')
-    #print(roman_to_decimal(roman_number))
-    assert roman_to_decimal("XV")==15
+    assert romano_a_decimal('XXIV')==24
+    #print(f'El número romano {numero_romano} en decimal es: {romano_a_decimal(numero_romano)}')
