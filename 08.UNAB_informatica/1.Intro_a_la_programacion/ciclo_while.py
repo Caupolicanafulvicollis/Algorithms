f7019ccfdea03e859f8664654369cca8a1c4a0ce
bombusdahlbomii <guillermo.vidal.astudillo@gmail.com>
2024-07-10 21:45:49 -0400

    Create ciclo_while.py

---
+++ 08.UNAB_informatica/1.Intro_a_la_programacion/ciclo_while.py
@@ -0,0 +1,14 @@
+#logica de un sumador
+contador=0 #para contar los numeros
+sumador=0 #para sumar los numeros
+#captura de datos y procesamiento
+while (n!=0):
+    n=int(input("Indique un numero: "))
+    if n!=0:
+        contador=contador+1
+        sumador=sumador+n
+    else:
+        print("Cantidad de numeros",contador)
+        print("Suma de los numeros",sumador)
+
+
