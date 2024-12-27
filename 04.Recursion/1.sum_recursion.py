29e825c96f5f8ef73c47f8cf1e920db698e8642e
Caupolicana fulvicollis <45744107+Caupolicanafulvicollis@users.noreply.github.com>
2024-12-26 23:40:48 -0300

    Delete 04.Recursion directory

--- 04.Recursion/1.sum_recursion.py
+++
@@ -1,7 +0,0 @@
-def sumalista(listaNumeros):
-   if len(listaNumeros) == 1:
-        return listaNumeros[0]
-   else:
-        return listaNumeros[0] + sumalista(listaNumeros[1:])
-
-print(sumalista([1,3,5,7,9]))
--- 04.Recursion/images/README.md
+++
@@ -1,2 +0,0 @@
-# Images
-En esta carpeta se enceuntran las imagenes con contenidos y explicaciones de los ejemplos de la programación recursiva
