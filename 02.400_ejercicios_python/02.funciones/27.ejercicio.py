edec26788fe23dc7afeceeb8a9af928d75efd9a4
diadasiachilensis <guillermo.vidal.astudillo@gmail.com>
2025-01-29 15:53:50 -0300

    27

---
+++ 02.400_ejercicios_python/02.funciones/27.ejercicio.py
@@ -0,0 +1,10 @@
+def max(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+if __name__ == "__main__":
+    num1=input("Ingrese el primer número: ")
+    num2=input("Ingrese el segundo número: ")
+    max(num1,num2)
