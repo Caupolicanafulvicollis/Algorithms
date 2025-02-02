666e48bbe1f0911abfff4f0d9bf0b8a180decdde
diadasiachilensis <guillermo.vidal.astudillo@gmail.com>
2025-02-01 22:48:41 -0300

    27 middle

--- 02.400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/27a.ejercicio.py
+++ 02.400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/27a.ejercicio.py
@@ -1,14 +1,33 @@
+def menu(a):
+    print("Agregar listas")
+    validar_numero(a)
+    crear_lista(a)
 
-
+def validar_numero(a):
+    if isinstance(a,int):
+        
 
 def crear_lista(a):
     list1=[]
-    while True:
-        a=int(input("ingresar un numero para agregar a la lista: "))
-        if a != 0:
-            list1.append(a)=list1
-        else: 
-            return False
+    try:
+        while True:
+            if isinstance(a,int):
+                print(f"El dato {a} ingresado es un int")
+                if a != 0:
+                    list1.append(a)
+                else: 
+                    return False
+                return False
+            else: 
+                print(f"El dato ingresado {a} no es un int")
+            return True
+    except Exception as e:
+        print(f"Error: {e}")
+        
+
+    pass
+        
 
 if __name__ == "__main__":
-    
+    num1=int(input("Ingresar un int para agregar a la lista: "))
+    menu(num1)
