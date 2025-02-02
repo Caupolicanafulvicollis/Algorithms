87724d2a506768ce9d22f30c628b195ae5c271b9
diadasiachilensis <guillermo.vidal.astudillo@gmail.com>
2025-02-01 22:29:38 -0300

    14 changes

--- 02.400_ejercicios_python/02.funciones/14.ejercicio.py
+++ 02.400_ejercicios_python/02.funciones/14.ejercicio.py
@@ -1,6 +1,6 @@
 """Crea un función EscribirCentrado, que reciba como parámetro un texto y lo escriba centrado en pantalla (suponiendo una anchura de 80 columnas;
 pista: deberás escribir 40 - longitud/2 espacios antes del texto). Además subraya el mensaje utilizando el carácter =."""
-def EscribirCentrado(texto):
+def escribir_centrado(texto):
     ancho = 80
     espacios = (ancho - len(texto))//2
     texto_centrado = ' '*espacios+texto
@@ -11,4 +11,4 @@ def EscribirCentrado(texto):
 
 
 if __name__ == "__main__":
-    EscribirCentrado("Bienvenido al mundo de Python")
+    escribir_centrado("Bienvenido al mundo de Python")
