4884ad704b15005190faab1fd873477eb3bf066b
diadasiachilensis <guillermo.vidal.astudillo@gmail.com>
2025-02-01 22:28:33 -0300

    37 start

---
+++ 02.400_ejercicios_python/03.listas-tuplas-conjuntos-diccionarios/27a.ejercicio.py
@@ -0,0 +1,14 @@
+
+
+
+def crear_lista(a):
+    list1=[]
+    while True:
+        a=int(input("ingresar un numero para agregar a la lista: "))
+        if a != 0:
+            list1.append(a)=list1
+        else: 
+            return False
+
+if __name__ == "__main__":
+    
